0474412032057aeb4c983ea13e403295d4ae305e
Paweł Szumański <pawel.szumanski@outlook.com>
2024-05-13 17:50:20 +0200

    add UserCreate and TokenModel

--- src/schemas/users.py
+++ src/schemas/users.py
@@ -47,3 +47,14 @@ class UserDb(BaseModel):
 
     class Config:
         from_attributes = True
+
+
+class UserCreate(BaseModel):
+    user: UserDb
+    detail: str = "User successfully created"
+
+
+class TokenModel(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
