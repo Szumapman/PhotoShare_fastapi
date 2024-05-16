a16b21a0f31923e31ade2dd3eb642736095b4d2c
Paweł Szumański <pawel.szumanski@outlook.com>
2024-05-16 19:35:24 +0200

    Update constant.py

--- src/conf/constant.py
+++ src/conf/constant.py
@@ -10,6 +10,7 @@ ROLE_STANDARD = "standard"
 API_V1 = "/api/v1"
 API = API_V1
 AUTH = "/auth"
+USERS = "/users"
 
 ACCESS_TOKEN = "access_token"
 REFRESH_TOKEN = "refresh_token"
@@ -27,12 +28,28 @@ HTTP_401_UNAUTHORIZED_DETAILS = [
     LOG_IN_AGAIN,
 ]
 
-TOKEN_NOT_FOUND = "Token not found"
+FORBIDDEN_OPERATION_ON_ADMIN_ACCOUNT = (
+    "Only admin can perform operations on users with admin role."
+)
+FORBIDDEN_FOR_USER = "Only admin or moderator can perform this operation."
+FORBIDDEN_FOR_USER_AND_MODERATOR = "Only admin can perform this operation."
+BANNED_USER = "User is banned."
 
-HTTP_404_NOT_FOUND_DETAILS = [TOKEN_NOT_FOUND]
+HTTP_403_FORBIDDEN_DETAILS = [
+    FORBIDDEN_OPERATION_ON_ADMIN_ACCOUNT,
+    FORBIDDEN_FOR_USER,
+    FORBIDDEN_FOR_USER_AND_MODERATOR,
+    BANNED_USER,
+]
+
+
+TOKEN_NOT_FOUND = "Token not found"
+USER_NOT_FOUND = "User not found"
+
+HTTP_404_NOT_FOUND_DETAILS = [TOKEN_NOT_FOUND, USER_NOT_FOUND]
 
 TOKEN_DELETED = "Token deleted"
 
-REDIS_EXPIRE = 60 * 15
-ACCESS_TOKEN_EXPIRE = 15
-REFRESH_TOKEN_EXPIRE = 7
+REDIS_EXPIRE = 60 * 15  # seconds
+ACCESS_TOKEN_EXPIRE = 15  # minutes
+REFRESH_TOKEN_EXPIRE = 7  # days
