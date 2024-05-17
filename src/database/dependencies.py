5dc147834e918f92e0fbd908867a8b3615d3802c
Paweł Szumański <pawel.szumanski@outlook.com>
2024-05-17 12:08:44 +0200

    add get_avatar_provider

--- src/database/dependencies.py
+++ src/database/dependencies.py
@@ -1,6 +1,12 @@
 from src.database.db import get_db
 from src.repository.abstract import AbstractUserRepo
 from src.repository.users import PostgresUserRepo
+from src.services.abstract import AbstractAvatarProvider
+from src.services.avatar import AvatarProviderGravatar
+
+
+def get_avatar_provider() -> AbstractAvatarProvider:
+    return AvatarProviderGravatar()
 
 
 def get_user_repository() -> AbstractUserRepo:
