4c1cd31448a8b01e408fb68c1285a992a946d469
Paweł Szumański <pawel.szumanski@outlook.com>
2024-05-17 12:09:16 +0200

    Update abstract.py

--- src/services/abstract.py
+++ src/services/abstract.py
@@ -2,6 +2,7 @@ import abc
 
 
 class AbstractAvatarProvider(abc.ABC):
+
     @abc.abstractmethod
-    def get_avatar(self, size: int):
+    def get_avatar(self, email: str, size: int):
         pass
