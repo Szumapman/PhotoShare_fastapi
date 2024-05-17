a9b081e590b45c79b6662a2c30f429273b34267a
Paweł Szumański <pawel.szumanski@outlook.com>
2024-05-17 12:09:19 +0200

    Update avatar.py

--- src/services/avatar.py
+++ src/services/avatar.py
@@ -4,14 +4,10 @@ from src.services.abstract import AbstractAvatarProvider
 
 
 class AvatarProviderGravatar(AbstractAvatarProvider):
-    def __init__(self, email: str):
+
+    def get_avatar(self, email: str, size: int) -> str | None:
         try:
             self.gravatar = Gravatar(email)
-        except Exception as e:
-            print(e)
-
-    def get_avatar(self, size: int) -> str | None:
-        try:
             return self.gravatar.get_image(size=size)
         except Exception as e:
             print(e)
