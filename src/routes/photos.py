993a2836904e9e469d4f06eecb899da328078b99
Paweł Szumański <pawel.szumanski@outlook.com>
2024-06-08 10:29:52 +0200

    add get photo

--- src/routes/photos.py
+++ src/routes/photos.py
@@ -3,10 +3,8 @@ from fastapi import (
     Depends,
     UploadFile,
     File,
-    Form,
     HTTPException,
     status,
-    Query,
 )
 
 from src.database.models import User
@@ -18,8 +16,7 @@ from src.database.dependencies import get_photo_repository
 from src.database.dependencies import get_photo_storage_provider
 from src.routes.auth import is_current_user_logged_in
 from src.conf.errors import PhotoStorageProviderError
-from src.conf.constant import PHOTOS
-
+from src.conf.constant import PHOTOS, PHOTO_NOT_FOUND, HTTP_404_NOT_FOUND_DETAILS
 
 router = APIRouter(prefix=PHOTOS, tags=["photos"])
 
@@ -47,7 +44,20 @@ async def create_photo(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Photo storage provider error: " + str(e),
         )
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
-        )
+
+
+@router.get("/{photo_id}", response_model=PhotoOut)
+async def get_photo(
+    photo_id: int,
+    current_user: User = Depends(auth_service.get_current_user),
+    photo_repo: AbstractPhotoRepo = Depends(get_photo_repository),
+):
+    if await is_current_user_logged_in(current_user):
+        photo = await photo_repo.get_photo_by_id(photo_id)
+        print(photo)
+        if photo in HTTP_404_NOT_FOUND_DETAILS:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=photo,
+            )
+        return photo
