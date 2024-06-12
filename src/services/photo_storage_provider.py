f1afca91a3b2fc6f163e5cf870127d4d5f118d0a
Paweł Szumański <pawel.szumanski@outlook.com>
2024-06-12 10:25:16 +0200

    add try - except

--- src/services/photo_storage_provider.py
+++ src/services/photo_storage_provider.py
@@ -62,7 +62,11 @@ class CloudinaryPhotoStorageProvider(AbstractPhotoStorageProvider):
         return await self._upload(qr_buffer, qr_code=True)
 
     async def delete_photo(self, photo: Photo):
-        photo_public_id = f"{CLOUDINARY_PHOTO_PUBLIC_ID_PREFIX}/{photo.photo_url.split('/')[-1].split('.')[0]}"
-        cloudinary.uploader.destroy(photo_public_id, invalidate=True)
-        qrcode_public_id = f"{CLOUDINARY_QR_PUBLIC_ID_PREFIX}/{photo.qr_url.split('/')[-1].split('.')[0]}"
-        cloudinary.uploader.destroy(qrcode_public_id, invalidate=True)
+        try:
+            photo_public_id = f"{CLOUDINARY_PHOTO_PUBLIC_ID_PREFIX}/{photo.photo_url.split('/')[-1].split('.')[0]}"
+            cloudinary.uploader.destroy(photo_public_id, invalidate=True)
+            qrcode_public_id = f"{CLOUDINARY_QR_PUBLIC_ID_PREFIX}/{photo.qr_url.split('/')[-1].split('.')[0]}"
+            cloudinary.uploader.destroy(qrcode_public_id, invalidate=True)
+        except Exception as e:
+            logger.error(e)
+            raise PhotoStorageProviderError(e)
