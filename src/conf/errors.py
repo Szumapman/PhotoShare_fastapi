364288dbc38f710bf317a445e75242b14f703d6b
Paweł Szumański <pawel.szumanski@outlook.com>
2024-07-01 12:18:47 +0200

    Update errors.py

--- src/conf/errors.py
+++ src/conf/errors.py
@@ -1,2 +1,20 @@
 class PhotoStorageProviderError(Exception):
     pass
+
+
+class NotFoundError(Exception):
+    def __init__(self, message=None, detail=None):
+        super().__init__(message)
+        self.detail = detail
+
+
+class ForbiddenError(Exception):
+    def __init__(self, message=None, detail=None):
+        super().__init__(message)
+        self.detail = detail
+
+
+class UnauthorizedError(Exception):
+    def __init__(self, message=None, detail=None):
+        super().__init__(message)
+        self.detail = detail
