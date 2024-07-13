4220090ecf67b1813aff999a62102701d6b21a16
Paweł Szumański <pawel.szumanski@outlook.com>
2024-07-13 12:42:57 +0200

    refactor to use error handlers from main, use model_validate and small bug fix

--- src/routes/users.py
+++ src/routes/users.py
@@ -42,19 +42,12 @@ async def get_users(
     current_user: User = Depends(auth_service.get_current_user),
     user_repo: AbstractUserRepo = Depends(get_user_repository),
 ):
-    try:
-        users = await user_repo.get_users()
-        if current_user.role == ROLE_ADMIN:
-            return [UserDb.from_orm(user) for user in users]
-        elif current_user.role == ROLE_MODERATOR:
-            return [UserModeratorView.from_orm(user) for user in users]
-        return [UserPublic.from_orm(user) for user in users]
-    except UnauthorizedError as e:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=e.detail)
-    except ForbiddenError as e:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=e.detail)
-    except NotFoundError as e:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=e.detail)
+    users = await user_repo.get_users()
+    if current_user.role == ROLE_ADMIN:
+        return [UserDb.model_validate(user) for user in users]
+    elif current_user.role == ROLE_MODERATOR:
+        return [UserModeratorView.from_orm(user) for user in users]
+    return [UserPublic.model_validate(user) for user in users]
 
 
 @router.get("/{user_id}", response_model=UserDb | UserModeratorView | UserPublic)
@@ -63,19 +56,12 @@ async def get_user(
     current_user: User = Depends(auth_service.get_current_user),
     user_repo: AbstractUserRepo = Depends(get_user_repository),
 ):
-    try:
-        user = await user_repo.get_user_by_id(user_id)
-        if current_user.role == ROLE_ADMIN or current_user.id == user_id:
-            return UserDb.from_orm(user)
-        elif current_user.role == ROLE_MODERATOR:
-            return UserModeratorView.from_orm(user)
-        return UserPublic.from_orm(user)
-    except UnauthorizedError as e:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=e.detail)
-    except ForbiddenError as e:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=e.detail)
-    except NotFoundError as e:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=e.detail)
+    user = await user_repo.get_user_by_id(user_id)
+    if current_user.role == ROLE_ADMIN or current_user.id == user_id:
+        return UserDb.model_validate(user)
+    elif current_user.role == ROLE_MODERATOR:
+        return UserModeratorView.model_validate(user)
+    return UserPublic.model_validate(user)
 
 
 @router.patch("/", response_model=UserInfo)
@@ -85,29 +71,20 @@ async def update_user(
     user_repo: AbstractUserRepo = Depends(get_user_repository),
     password_handler: AbstractPasswordHandler = Depends(get_password_handler),
 ):
-    try:
-        if new_user_data.username != current_user.username:
-            if await user_repo.get_user_by_username(new_user_data.username):
-                raise HTTPException(
-                    status_code=status.HTTP_409_CONFLICT, detail=USERNAME_EXISTS
-                )
-        elif new_user_data.email != current_user.email:
-            if await user_repo.get_user_by_email(new_user_data.email):
-                raise HTTPException(
-                    status_code=status.HTTP_409_CONFLICT, detail=EMAIL_EXISTS
-                )
-        new_user_data.password = password_handler.get_password_hash(
-            new_user_data.password
-        )
-        user = await user_repo.update_user(new_user_data, current_user.id)
-        await auth_service.update_user_in_redis(user.email, user)
-        return UserInfo(user=UserDb.model_validate(user), detail=USER_UPDATE)
-    except UnauthorizedError as e:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=e.detail)
-    except ForbiddenError as e:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=e.detail)
-    except NotFoundError as e:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=e.detail)
+    if new_user_data.username != current_user.username:
+        if await user_repo.get_user_by_username(new_user_data.username):
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT, detail=USERNAME_EXISTS
+            )
+    elif new_user_data.email != current_user.email:
+        if await user_repo.get_user_by_email(new_user_data.email):
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT, detail=EMAIL_EXISTS
+            )
+    new_user_data.password = password_handler.get_password_hash(new_user_data.password)
+    user = await user_repo.update_user(new_user_data, current_user.id)
+    await auth_service.update_user_in_redis(user.email, user)
+    return UserInfo(user=UserDb.model_validate(user), detail=USER_UPDATE)
 
 
 @router.patch("/avatar", response_model=UserInfo)
@@ -131,18 +108,13 @@ async def delete_user(
     current_user: User = Depends(auth_service.get_current_user),
     user_repo: AbstractUserRepo = Depends(get_user_repository),
 ):
-    try:
-        if current_user.role == ROLE_ADMIN or current_user.id == user_id:
-            user = await user_repo.get_user_by_id(user_id)
-            user = await user_repo.delete_user(user.id)
-            await auth_service.delete_user_from_redis(user.email)
-            return UserInfo(user=UserDb.model_validate(user), detail=USER_DELETE)
-    except UnauthorizedError as e:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=e.detail)
-    except ForbiddenError as e:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=e.detail)
-    except NotFoundError as e:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=e.detail)
+    if current_user.role == ROLE_ADMIN or current_user.id == user_id:
+        user = await user_repo.get_user_by_id(user_id)
+        user = await user_repo.delete_user(user.id)
+        await auth_service.delete_user_from_redis(user.email)
+        return UserInfo(user=UserDb.model_validate(user), detail=USER_DELETE)
+    else:
+        raise ForbiddenError(detail=FORBIDDEN_FOR_USER_AND_MODERATOR)
 
 
 @router.patch("/active_status/{user_id}", response_model=UserInfo)
@@ -152,22 +124,13 @@ async def set_active_status(
     current_user: User = Depends(auth_service.get_current_user),
     user_repo: AbstractUserRepo = Depends(get_user_repository),
 ):
-    try:
-        if current_user.role not in [ROLE_ADMIN, ROLE_MODERATOR]:
-            raise ForbiddenError(FORBIDDEN_FOR_USER)
-        user = await user_repo.set_user_active_status(
-            user_id, active_status, current_user
-        )
-        return UserInfo(
-            user=UserDb.from_orm(user),
-            detail=f"User status set to {'active' if active_status.is_active else 'banned'}.",
-        )
-    except UnauthorizedError as e:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=e.detail)
-    except ForbiddenError as e:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=e.detail)
-    except NotFoundError as e:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=e.detail)
+    if current_user.role not in [ROLE_ADMIN, ROLE_MODERATOR]:
+        raise ForbiddenError(FORBIDDEN_FOR_USER)
+    user = await user_repo.set_user_active_status(user_id, active_status, current_user)
+    return UserInfo(
+        user=UserDb.from_orm(user),
+        detail=f"User status set to {'active' if active_status.is_active else 'banned'}.",
+    )
 
 
 @router.patch("/set_role/{user_id}", response_model=UserInfo)
@@ -177,17 +140,10 @@ async def set_role(
     current_user: User = Depends(auth_service.get_current_user),
     user_repo: AbstractUserRepo = Depends(get_user_repository),
 ):
-    try:
-        if current_user.role != ROLE_ADMIN:
-            raise ForbiddenError(FORBIDDEN_FOR_USER_AND_MODERATOR)
-        user = await user_repo.set_user_role(user_id, role)
-        await auth_service.update_user_in_redis(user.email, user)
-        return UserInfo(
-            user=UserDb.from_orm(user), detail=f"User role set to {role.role.value}."
-        )
-    except UnauthorizedError as e:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=e.detail)
-    except ForbiddenError as e:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=e.detail)
-    except NotFoundError as e:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=e.detail)
+    if current_user.role != ROLE_ADMIN:
+        raise ForbiddenError(FORBIDDEN_FOR_USER_AND_MODERATOR)
+    user = await user_repo.set_user_role(user_id, role)
+    await auth_service.update_user_in_redis(user.email, user)
+    return UserInfo(
+        user=UserDb.model_validate(user), detail=f"User role set to {user.role}."
+    )
