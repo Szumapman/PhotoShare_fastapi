22353de19939060e245aefc3dd654361e1678341
Paweł Szumański <pawel.szumanski@outlook.com>
2024-08-06 17:01:05 +0200

    add logger

--- src/database/db.py
+++ src/database/db.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from src.conf.config import settings
+from src.conf.logger import logger
 
 SQLALCHEMY_DATABASE_URL = settings.sqlalchemy_database_url
 
@@ -26,7 +27,7 @@ def get_db():
     try:
         yield db
     except Exception as e:
-        print(e)
+        logger.exception(e)
         db.rollback()
     finally:
         db.close()
